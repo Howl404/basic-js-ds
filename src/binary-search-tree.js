53835e6a48e408d67de0c8a433d6e3fe08bd4109
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-04-13 16:48:35 +0300

    1

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, checkForNotThrowingErrors } = require('../extensions/index.js');
 
 // const { Node } = require('../extensions/list-tree.js');
 
@@ -6,41 +6,144 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.rootElement = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootElement
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootElement = addWithin(this.rootElement, data)
+
+    function addWithin(node, data){
+      if (!node) {
+        return new Node(data);
+      } else if (node.data === data) {
+        return node;
+      }
+
+      if(data < node.data) {
+        node.left = addWithin(node.left, data);
+      } else {
+        node.right = addWithin(node.right, data)
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithin(this.rootElement, data)
+
+    function searchWithin(node, data) {
+      if(!node) {
+        return false;
+      } else if (node.data === data) {
+        return true;
+      }
+
+      if(data < node.data) {
+        return searchWithin(node.left, data);
+      } else return searchWithin(node.right, data);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data ) {
+    return searchWithin(this.rootElement, data)
+
+    function searchWithin(node, data) {
+      if(!node) {
+        return null;
+      } else if (node.data === data) {
+        return node;
+      }
+
+      if(data < node.data) {
+        return searchWithin(node.left, data);
+      } else return searchWithin(node.right, data);
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.rootElement = removeNode(this.rootElement, data);
+
+    function removeNode(node, data) {
+      if(!node) {
+        return null;
+      }
+
+      if(data < node.data) {
+        node.left = removeNode(node.left, data)
+        return node;
+      } else if (data > node.data) {
+        node.right = removeNode(node.right, data)
+        return node;
+      } else {
+        if(!node.left && !node.right) {
+          return null;
+        }
+
+        if(!node.left) {
+          node = node.right
+          return node;
+        }
+        
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minimumRightData = node.right;
+        while(minimumRightData.left) {
+          minimumRightData = minimumRightData.left;
+        }
+
+        node.data = minimumRightData.data;
+        
+        node.right = removeNode(node.right, minimumRightData.data);
+        
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.rootElement) {
+      return;
+    }
+
+    let node = this.rootElement;
+    while(node.left) {
+      node = node.left
+    }
+
+    return node.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if(!this.rootElement) {
+      return;
+    }
+
+    let node = this.rootElement;
+    while(node.right) {
+      node = node.right
+    }
+
+    return node.data
   }
 }
 
